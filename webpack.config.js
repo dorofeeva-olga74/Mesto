3c08f2743676cf21944d3669b80c39049186069b
Olga Gorobets <dorofeeva.olga74@mail.ru>
2023-08-18 23:56:28 +0900

    Mesto28

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,46 @@
 // webpack.config.js
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path'); // подключаем path к конфигу вебпак
+const HtmlWebpackPlugin = require('html-webpack-plugin'); // подключите плагинs
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: {
     main: './src/index.js'
-  },
+  },// указали первое место, куда заглянет webpack, — файл index.js в папке src
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'dist'),//Ему переданы два аргумента: ссылка на текущую папку __dirname
+                                        // и относительный путь к точке выхода
     filename: 'main.js',
     publicPath: '',
-  },
-  mode: 'development',
+  },// указали в какой файл будет собираться весь js и дали ему имя 
+  // переписали точку выхода, используя утилиту path 
+  mode: 'development', // добавили режим разработчика
   devServer: {
-    static: path.resolve(__dirname, './dist'),
-    open: true,
-    compress: true,
-    port: 8080
+    static: path.resolve(__dirname, './dist'), // путь, куда "смотрит" режим разработчика
+    compress: true, // это ускорит загрузку в режиме разработки
+    port: 8080, // порт, чтобы открывать сайт по адресу localhost:8080, но можно поменять порт
+
+    open: true // сайт будет открываться сам при запуске npm run dev
   },
   module: {
-    rules: [{
+    rules: [{// rules — это массив правил
+      // добавим в него объект правил для бабеля
+      // регулярное выражение, которое ищет все js файлы
         test: /\.js$/,
+        // при обработке этих файлов нужно использовать babel-loader
         use: 'babel-loader',
+        // исключает папку node_modules, файлы в ней обрабатывать не нужно
         exclude: '/node_modules/'
-      },
-      {
+      },// добавили правило для обработки файлов
+      {// регулярное выражение, которое ищет все файлы с такими расширениями
         test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
         type: 'asset/resource',
       },
-      {
+      {// применять это правило только к CSS-файлам
         test: /\.css$/,
+        // при обработке этих файлов нужно использовать
+    // MiniCssExtractPlugin.loader и css-loader
         use: [MiniCssExtractPlugin.loader, {
             loader: 'css-loader',
             options: {
@@ -45,11 +54,11 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html'
+      template: './src/index.html'// путь к файлу index.html
     }),
     new CleanWebpackPlugin(),
-    new MiniCssExtractPlugin(),
-
+    new MiniCssExtractPlugin(),// подключение плагина для объединения файлов
+] ,
   ]
 } 
 
