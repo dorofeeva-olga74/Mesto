1810225a35a69473d3343e47d2e625cf79b6e12a
Olga Gorobets <dorofeeva.olga74@mail.ru>
2023-08-19 19:10:58 +0900

    Mesto 29

--- webpack.config.js
+++ webpack.config.js
@@ -54,12 +54,12 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html'// путь к файлу index.html
+      template: "./src/index.html",// путь к файлу index.html
     }),
     new CleanWebpackPlugin(),
     new MiniCssExtractPlugin(),// подключение плагина для объединения файлов
-] ,
-  ]
+  ],  
+  
 } 
 
 // переписали точку выхода, используя утилиту path
